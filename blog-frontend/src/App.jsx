d9b8fa891ff14d17bea8d86410d39681903a7a66
Diana <dianateresalopezvega@gmail.com>
2025-09-23 19:51:24 -0600

    agregue

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -1,11 +1,26 @@
+import { BrowserRouter } from 'react-router-dom'
+import BlogRoutes from './routes/BlogRoutes'
+import Header from './components/Header/Header'
 import './App.css'
-import NewPost from './pages/NewPost/Newpost.jsx'
 
 function App () {
   return (
-    <>
-      <NewPost />
-    </>
+    <BrowserRouter>
+      <Header />
+      <div className='app'>
+        <main className='app-main'>
+          <div className='container'>
+            <BlogRoutes />
+          </div>
+        </main>
+
+        <footer className='app-footer'>
+          <div className='container'>
+            <p>© 2024 Mi Blog. Todos los derechos reservados.</p>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   )
 }
 
